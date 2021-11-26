679a3aeb372bb2717db55e7c0dd490aa3293f889
Vice10 <42076380+Vice10@users.noreply.github.com>
2021-11-26 22:55:22 +0300

    Add files via upload

---
+++ IT_stage_22/TableEntry.java
@@ -0,0 +1,44 @@
+package com.it;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.w3c.dom.Attr;
+
+import java.util.ArrayList;
+
+public class TableEntry {
+    private ArrayList<Attribute> cells;
+    TableEntry(ArrayList<Attribute> schema){
+        cells = new ArrayList<>();
+        for(var attr : schema)
+            cells.add(new Attribute(attr));
+    }
+    public boolean setVal(Attribute newVal){
+        for (var cell : cells)
+            if(cell.name == newVal.name && cell.type == newVal.type) {
+                cell.setVal(newVal.getVal());
+                return true;
+            }
+        return false;
+    }
+    public boolean setVal(String attrName, AttributeType attrType, Object val){
+        for (var cell : cells)
+            if(cell.name == attrName && cell.type == attrType) {
+                cell.setVal(val);
+                return true;
+            }
+        return false;
+    }
+    public JSONArray toJson(){
+        JSONArray myCells = new JSONArray();
+        for(var cell : cells)
+            myCells.add(cell.getJsonFormatVal());
+        return myCells;
+    }
+    public String toJsonStr(){
+        JSONArray myCells = new JSONArray();
+        for(var cell : cells)
+            myCells.add(cell.getJsonFormatVal());
+        return myCells.toJSONString();
+    }
+}
---
+++ IT_stage_22/TableScheme.java
@@ -0,0 +1,30 @@
+package com.it;
+
+import java.util.ArrayList;
+public class TableScheme {
+    private String attrTypes;
+    private ArrayList<String> attrNames;
+    TableScheme(){}
+    TableScheme(String attrTypes, ArrayList<String> attrNames){
+        this.attrTypes = attrTypes;
+        this.attrNames = new ArrayList<>();
+        this.attrNames.addAll(attrNames);
+    }
+
+    public String getAttrTypes() {
+        return attrTypes;
+    }
+
+    public void setAttrTypes(String attrTypes) {
+        this.attrTypes = attrTypes;
+    }
+
+    public ArrayList<String> getAttrNames() {
+        return attrNames;
+    }
+
+    public void setAttrNames(ArrayList<String> attrNames) {
+        this.attrNames = new ArrayList<>();
+        this.attrNames.addAll(attrNames);
+    }
+}
